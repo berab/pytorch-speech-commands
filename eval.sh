c87345a7342a6718b1e17676eecf082c72e3b9ac
Renan Kilic <kilic@codesign99>
2022-01-14 18:12:46 +0100

    bash files

---
+++ eval.sh
@@ -0,0 +1,2 @@
+python test_speech_commands.py ./trained-models/1642087598753-vgg19_bn_sgd_plateau_bs96_lr1.0e-02_wd1.0e-02-best-acc.pth 
+
---
+++ train.sh
@@ -0,0 +1,2 @@
+python train_speech_commands.py --model=vgg19_bn --optim=sgd --lr-scheduler=plateau --learning-rate=0.01 --lr-scheduler-patience=5 --max-epochs=70 --batch-size=96
+
